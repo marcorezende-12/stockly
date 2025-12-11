2bc83d5f650778d31c8eece5b3ea0e96da164fdd
Marco Aurelio Rezende <marco.aurelio.rsilva7@gmail.com>
2025-12-11 14:39:17 -0300

    feat: function to add product to table summary added

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,5 +1,40 @@
-const SalesPage = () => {
-  return <h1>Sales Page</h1>;
+import { Button } from "../_components/ui/button";
+import { Sheet, SheetTrigger } from "../_components/ui/sheet";
+import { getProducts } from "../_data-access/product/get-product";
+import UpsertSheetContent from "./_components/upsert-sheet-content";
+
+const SalesPage = async () => {
+  const products = await getProducts();
+  const productOptions = products.map((product) => ({
+    label: product.name,
+    value: product.id,
+  }));
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de Vendas
+          </span>
+          <h2 className="text-xl font-semibold">Vendas</h2>
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>Nova venda</Button>
+          </SheetTrigger>
+
+          <UpsertSheetContent
+            products={products}
+            productOptions={productOptions}
+          />
+        </Sheet>
+      </div>
+      {/* <DataTable
+          columns={productTableColumns}
+          data={JSON.parse(JSON.stringify(products))}
+        /> */}
+    </div>
+  );
 };
 
 export default SalesPage;
